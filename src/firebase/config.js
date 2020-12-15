9e449cc0d2b154a85fc7c9ce755f728b42ea029e
RUSHESH SHARMA <sharmarushesh@gmail.com>
2020-12-15 13:08:47 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,6 @@ import 'firebase/storage';
 import 'firebase/firestore';
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBXcfa5uDYuPhULD4hyQ5nM5H6QX724GVk",
-  authDomain: "photoes-app-d440f.firebaseapp.com",
-  databaseURL: "https://photoes-app-d440f-default-rtdb.firebaseio.com",
-  projectId: "photoes-app-d440f",
-  storageBucket: "photoes-app-d440f.appspot.com",
-  messagingSenderId: "296078546019",
-  appId: "1:296078546019:web:f6aff14049cb804cbe1456",
-  measurementId: "G-ZFH4M62SEP"
 };
 
 // Initialize Firebase
